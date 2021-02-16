13dee80502df47fda6996048a6152b5f4b41e8aa
Ramon Lence <rlence86@gmail.com>
2021-02-16 19:55:09 +0100

    first commit

---
+++ src/main/java/com/jokill/jokillapi/JokillApiApplication.java
@@ -0,0 +1,13 @@
+package com.jokill.jokillapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokillApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokillApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jokill/jokillapi/JokillApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jokill.jokillapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokillApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
